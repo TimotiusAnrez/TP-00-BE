d4ea9b2babe61580f86903be68314dacac4e71db
unknown <anrez.t@gmail.com>
2021-11-08 22:49:20 +0700

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors);
 
 // const connectDB = require('./config/mongoDB');
 
